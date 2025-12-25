34f1b2f2c22e109a1f0b6c48354f3ea9489c2f53
Mohamed Ahmed <115600312+muhammedahmed20@users.noreply.github.com>
2025-12-25 19:27:28 +0200

    Update script.js3

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 let phones = JSON.parse(localStorage.getItem("phones")) || [
-  { name: "iPhone", price: 400, qty: 10 },
-  { name: "iPhone 11 Pro", price: 550, qty: 2 },
-  { name: "iPhone 12", price: 650, qty: 5 },
+  { name: "iphone", price: 400, qty: 10 },
+  { name: "iphone 11 pro", price: 550, qty: 2 },
+  { name: "iphone 12", price: 650, qty: 5 },
 ];
 
+
+let toggleBtn = document.querySelector(".toggle");
+let isDark = false;
+let body = document.querySelector("body");
+let header = document.querySelector("header");
+let mood = document.querySelector(".mood");
 let tbody = document.querySelector("tbody");
 
 let phoneName = document.querySelector("#phoneName");
@@ -13,76 +19,210 @@ let phoneQty = document.querySelector("#phoneQty");
 let updatePhoneName = document.querySelector("#updatePhoneName");
 let updatePhonePrice = document.querySelector("#updatePhonePrice");
 let updatePhoneQty = document.querySelector("#updatePhoneQty");
-let updatedIndex = null;
 
-function savePhones() {
-  localStorage.setItem("phones", JSON.stringify(phones));
-}
+let modalBody = document.querySelector(".modal-body");
+let updateSelect = document.querySelector(".form-select");
+let updateForm = document.querySelector("#updateForm");
 
-function showPhones() {
+let updatedIndex;
+
+let toggle = () => {
+  toggleBtn.classList.toggle("active");
+  if (isDark == false) {
+    body.style.backgroundColor = "#121212";
+    body.style.color = "#fff";
+    header.style.backgroundColor = "#121212";
+    header.style.color = "#fff";
+    mood.innerText = "Light ";
+    isDark = true;
+  } else {
+    body.style.backgroundColor = "#fff";
+    body.style.color = "#000";
+    header.style.backgroundColor = "#fff";
+    header.style.color = "#000";
+    mood.innerText = "Dark ";
+    isDark = false;
+  }
+
+  savePhones()
+};
+
+let showPhones = () => {
   tbody.innerHTML = "";
-  phones.forEach((phone, index) => {
+  phones.forEach((el, index) => {
     tbody.innerHTML += `
-      <tr>
-        <td>${index + 1}</td>
-        <td>${phone.name} <i class="fa-solid fa-pen-to-square text-secondary" onclick="editPhone(${index}, 'name')"></i></td>
-        <td>${phone.price} <i class="fa-solid fa-pen-to-square text-secondary" onclick="editPhone(${index}, 'price')"></i></td>
-        <td>${phone.qty} <i class="fa-solid fa-pen-to-square text-secondary" onclick="editPhone(${index}, 'qty')"></i></td>
-        <td>${phone.price * phone.qty}</td>
-        <td>
-          <button onclick="removePhone(${index})" class="btn"><i class="fa-solid fa-trash-can text-danger"></i></button>
-          <button class="btn" data-bs-toggle="modal" data-bs-target="#updateAll" onclick="sendPhoneInfo(${index})">
-            <i class="fa-solid fa-pen-to-square text-primary"></i>
-          </button>
-        </td>
-      </tr>
+    <tr>
+      <td class="col-1">${index + 1}</td>
+      <td class="col-4">
+        <div class=" d-flex justify-content-center align-items-center">
+        <p class="changeName mb-0 col-8">${el.name}</p>
+        <i type="button" id="editBtn" class="fa-solid fa-pen-to-square me-2 text-secondary" onclick="sendNameField(${index})"></i>
+        
+        </div>
+      </td>
+      <td class="col-2">
+        <div class=" d-flex justify-content-center align-items-center">
+        <p class="changePrice mb-0 col-8"> ${el.price}</p>
+        <i type="button" id="editBtn1" class="fa-solid fa-pen-to-square me-2 text-secondary" onclick="sendPriceField(${index})"></i>
+        </div>
+      </td>
+      <td class="col-2">
+        <div class=" d-flex justify-content-center align-items-center">
+        <p class="changeQty mb-0 col-8">${el.qty}</p>
+        <i type="button" id="editBtn2" class="fa-solid fa-pen-to-square me-2 text-secondary" onclick="sendQtyField(${index})"></i>
+        </div>
+      </td>
+      <td class="col-1">${el.qty * el.price}</td>
+      <td class="col-2">
+      <button onclick="removePhone(${index})" class="btn me-3">
+      <i class="fa-solid fa-trash-can text-danger"></i>
+      </button>
+      <button
+        type="button"
+        class="btn "
+        data-bs-toggle="modal"
+        data-bs-target="#updateAll"
+        onClick="sendPhoneInfo(${index})"
+      >
+          <i class="fa-solid fa-pen-to-square text-primary"></i>
+      </button>
+
+      </td>
+    </tr>
     `;
   });
-  savePhones();
-}
+  savePhones()
+};
 
-function addNewPhone() {
-  if (!phoneName.value || !phonePrice.value || !phoneQty.value) return alert("Enter all values");
-  phones.push({
-    name: phoneName.value,
-    price: +phonePrice.value,
-    qty: +phoneQty.value,
-  });
-  phoneName.value = "";
-  phonePrice.value = "";
-  phoneQty.value = "";
+let addNewPhone = () => {
+  let newName = phoneName;
+  let newPrice = phonePrice;
+  let newQty = phoneQty;
+  let newPhoneValue = {
+    name: newName.value,
+    price: +newPrice.value,
+    qty: +newQty.value,
+  };
+
+  phones.push(newPhoneValue);
   showPhones();
-}
 
-function removePhone(index) {
-  if (confirm("Are you sure?")) {
-    phones.splice(index, 1);
-    showPhones();
-  }
-}
+  newName.value = "";
+  newPrice.value = "";
+  newQty.value = "";
+  savePhones()
+};
 
-function sendPhoneInfo(index) {
-  updatedIndex = index;
+let removePhone = (index) => {
+  Swal.fire({
+    title: "Are you sure?",
+    text: "You won't be able to revert this!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Yes, delete it!",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire({
+        title: "Deleted!",
+        text: "Your file has been deleted.",
+        icon: "success",
+      });
+      phones.splice(index, 1);
+      showPhones();
+    }
+  });
+  savePhones()
+};
+
+let sendPhoneInfo = (index) => {
   updatePhoneName.value = phones[index].name;
   updatePhonePrice.value = phones[index].price;
   updatePhoneQty.value = phones[index].qty;
-}
 
-function updatePhone() {
-  if (updatedIndex === null) return;
+  updatedIndex = index;
+
+  savePhones()
+};
+
+let updatePhone = () => {
   phones[updatedIndex].name = updatePhoneName.value;
-  phones[updatedIndex].price = +updatePhonePrice.value;
-  phones[updatedIndex].qty = +updatePhoneQty.value;
-  updatedIndex = null;
+  phones[updatedIndex].price = updatePhonePrice.value;
+  phones[updatedIndex].qty = updatePhoneQty.value;
   showPhones();
-}
+  savePhones()
+};
 
-function editPhone(index, field) {
-  let newValue = prompt(`Enter new ${field}:`, phones[index][field]);
-  if (newValue === null) return;
-  if (field === "price" || field === "qty") newValue = +newValue;
-  phones[index][field] = newValue;
+let sendNameField = (index) => {
+  hideEditButton()
+
+  let changeName = document.querySelector(".changeName");
+  changeName.innerHTML = `
+  <div class="d-flex justify-content-center align-items-center">
+  <input class="nameField col-10 me-2" type="text" value="${phones[index].name}">
+  <i class="fa-solid fa-check text-success" type="button" onclick="updateName(${index}, this)"></i>
+  </div>
+  `;
+  savePhones()
+};
+let updateName = (index) => {
+  let nameField = document.querySelector(".nameField");
+  phones[index].name = nameField.value;
   showPhones();
+  savePhones()
+};
+
+let sendPriceField = (index) => {
+  hideEditButton()
+
+  let changePrice = document.querySelector(".changePrice");
+  changePrice.innerHTML = `
+  <div class="d-flex justify-content-center align-items-center">
+  <input class="priceField col-10" type="number" value="${phones[index].price}">
+  <i class="fa-solid fa-check text-success" type="button" onclick="updatePrice(${index}, this)"></i>
+  </div>
+  `;
+  savePhones()
+};
+let updatePrice = (index) => {
+  let priceField = document.querySelector(".priceField");
+  phones[index].price = priceField.value;
+  showPhones();
+  savePhones()
+};
+
+let sendQtyField = (index) => {
+  hideEditButton()
+  let changeQty = document.querySelector(".changeQty");
+  changeQty.innerHTML = `
+  <div class="d-flex justify-content-center align-items-center">
+  <input class="qtyField col-10" type="text" value="${phones[index].qty}">
+  <i class="fa-solid fa-check text-success" type="button" onclick="updateQty(${index}, this)"></i>
+  </div>
+  `;
+  savePhones()
+};
+let updateQty = (index) => {
+  let qtyField = document.querySelector(".qtyField");
+  phones[index].qty = qtyField.value;
+  savePhones()
+  showPhones();
+};
+
+let hideEditButton = () => {
+  let editBtn = document.querySelector("#editBtn");
+  let editBtn1 = document.querySelector("#editBtn1");
+  let editBtn2 = document.querySelector("#editBtn2");
+  editBtn.classList.add("d-none");
+  editBtn1.classList.add("d-none");
+  editBtn2.classList.add("d-none");
+  savePhones()
+};
+
+
+function savePhones() {
+  localStorage.setItem("phones", JSON.stringify(phones));
 }
 
 showPhones();
